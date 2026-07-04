961d4947f5041df1e995702e0f78afc3eeb235c8
aasthamahajan19 <mahajanaastha9172@gmail.com>
2026-07-04 13:45:58 +0530

    Add: validate-parentheses - submission-1

---
+++ Data Structures & Algorithms/validate-parentheses/submission-1.cpp
@@ -0,0 +1,23 @@
+class Solution {
+public:
+    bool isValid(string s) {
+        stack<char> st;
+        for(char c :s){
+            if(c=='(' || c=='{' || c=='['){
+                st.push(c);
+            }
+            else{
+                if(!st.empty()){
+                    if((c==')' && st.top()=='(') ||
+                       (c=='}' && st.top()=='{') ||
+                       (c==']' && st.top()=='[')){
+                        st.pop();
+                    }
+                    else return false;
+                }
+                else return false;
+            }
+        }
+        return st.empty();
+    }
+};
